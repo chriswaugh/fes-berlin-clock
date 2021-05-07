3206047736379a2c30ffc93b029352b4bfa8a867
Chris Waugh <chris@electricsheep.ltd>
2021-05-07 12:05:29 +0100

    Progress from 1st code dojo

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 module.exports = {
-    func: () => null
+    func: (time) => {
+        if (time === '00:00:00'){
+            return '0000';
+        } else if (time === '00:01:00') {
+            return '1000';
+        } else if (time === '00:02:00') {
+            return '1100';
+        } else if (time === '00:03:00') {
+            return '1110';
+        } else if (time === '00:04:00') {
+            return '1111';
+        } else if (time === '00:05:00') {
+            return '0000';
+        }
+    },
+    minutes: (time) => {
+        return parseInt(time.substring(3,5));
+    }    
 };
--- index.spec.js
+++ index.spec.js
@@ -2,8 +2,58 @@ const SUT = require('./index');
 
 describe('Berlin Clock', () => {
 
-    it('will do nothing', () => {
-        expect(SUT.func()).toBeNull();
+    it('will return 0000 for no minutes', () => {
+        const time = '00:00:00'
+        expect(SUT.func(time)).toBe('0000');
+    });
+
+    it('will return 1000 for one minutes', () => {
+        const time = '00:01:00'
+        expect(SUT.func(time)).toBe('1000');
+    });
+
+    it('will return 1100 for two minutes', () => {
+        const time = '00:02:00'
+        expect(SUT.func(time)).toBe('1100');
+    });
+
+    it('will return 1110 for three minutes', () => {
+        const time = '00:03:00'
+        expect(SUT.func(time)).toBe('1110');
+    });
+    
+    it('will return 1111 for four minutes', () => {
+        const time = '00:04:00'
+        expect(SUT.func(time)).toBe('1111');
+    });
+    
+    it('will return 0000 for five minutes', () => {
+        const time = '00:05:00'
+        expect(SUT.func(time)).toBe('0000');
+    });
+
+});
+
+describe('minutes', () => {
+
+    it('will return 0 for 0 minutes', () => {
+        const time = '00:00:00'
+        expect(SUT.minutes(time)).toBe(0);
+    });
+
+    it('will return 1 for 1 minutes', () => {
+        const time = '00:01:00'
+        expect(SUT.minutes(time)).toBe(1);
+    });
+
+    it('will return 2 for 2 minutes', () => {
+        const time = '00:02:00'
+        expect(SUT.minutes(time)).toBe(2);
+    });
+
+    it('will return 33 for 33 minutes', () => {
+        const time = '00:33:00'
+        expect(SUT.minutes(time)).toBe(33);
     });
 
 });
